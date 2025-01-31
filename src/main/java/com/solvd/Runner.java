9b03e96d417d5ce6559f5600dc15f4ea9ff7bab0
spitsynv2 <vadym.spitsyn@gmail.com>
2025-01-31 13:13:21 +0100

    Cleaned debug the comments

--- src/main/java/com/solvd/Runner.java
+++ src/main/java/com/solvd/Runner.java
@@ -11,42 +11,7 @@ public class Runner
 
     public static void main(String[] args )
     {
-        /*
-        String URL = "jdbc:mysql://localhost:3306/banking_service";
-        String USER = "root";
-        String PASSWORD = "root";
-        Connection connection;
-
-        try {
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-         */
-
-        //CustomerDAOImpl customerDAO = new CustomerDAOImpl(connection);
-        //CustomerRepresentativeDAOImpl customerRepresentativeDAO = new CustomerRepresentativeDAOImpl(connection);
-        //AddressDAOImpl addressDAO = new AddressDAOImpl(connection);
-        //AccountDAOImpl accountDAO = new AccountDAOImpl(connection);
-        //TransactionDAOImpl transactionDAO = new TransactionDAOImpl(connection);
-        //LoanDAOImpl loanDAO = new LoanDAOImpl(connection);
-        //DepositDAO depositDAO = new DepositDAO(connection);
-        //CardDAOImpl cardDAO = new CardDAOImpl(connection);
-
-        //log.info(customerDAO.readById(1L));
-        //CompanyCustomer companyCustomer = (CompanyCustomer) customerDAO.readById(3L);
-        //log.info((companyCustomer.getRepresentatives()));
-        //log.info((customerRepresentativeDAO.readAllByForeignKeyId(3L)));
-
-        //log.info(addressDAO.readAllByForeignKeyId(1L));
-        //log.info(accountDAO.readAllByForeignKeyId(1L));
-        //log.info(transactionDAO.readAllByForeignKeyId(1L));
-        //log.info(loanDAO.readAllByForeignKeyId(3L));
-        //log.info(depositDAO.readAllByForeignKeyId(1L));
-        //log.info(cardDAO.readAllByForeignKeyId(1L));
-
         Customer customer = CustomerAccountService.getCustomerFromDataBase(4L);
         log.info(customer);
-
     }
 }
